b65adef2d8f0ab3a5fffe438ed14ab6b0906601c
sameerpanthi <75129693+sameerpanthi@users.noreply.github.com>
2021-07-14 19:49:12 +0530

    Update admin_rights.py

--- ShasaBot/helper_extra/admin_rights.py
+++ ShasaBot/helper_extra/admin_rights.py
@@ -5,8 +5,6 @@ def user_can_promote(chat: Chat, user: User, bot_id: int) -> bool:
     return chat.get_member(user.id).can_promote_members
 
 
-def user_can_ban(chat: Chat, user: User, bot_id: int) -> bool:
-    return chat.get_member(user.id).can_restrict_members
 
 
 def user_can_pin(chat: Chat, user: User, bot_id: int) -> bool:
