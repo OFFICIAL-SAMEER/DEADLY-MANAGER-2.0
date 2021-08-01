b824345798ad53da83a936ef168550ecf64d0120
sameerpanthi <sameerpanthi@users.noreply.github.com>
2021-08-01 14:04:45 +0000

    Things

--- DeadlyBot/modules/helper_funcs/misc.py
+++ DeadlyBot/modules/helper_funcs/misc.py
@@ -69,7 +69,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     if len(pairs) > 7:
         pairs = pairs[modulo_page * 7:7 * (modulo_page + 1)] + [
             (EqInlineKeyboardButton("👈🏻", callback_data="{}_prev({})".format(prefix, modulo_page)),
-                EqInlineKeyboardButton("🌹", callback_data="deadly_back"),
+                EqInlineKeyboardButton("🌹", callback_data="ShasaBot_back"),
              EqInlineKeyboardButton("👉🏻", callback_data="{}_next({})".format(prefix, modulo_page)))]
 
     else:
